08dfc99dc051aed7696a8d12cefdbf87b9004222
Ausra Bickuviene <bickuviene.ausra@gmail.com>
2022-12-01 22:18:01 +0200

    Added menu choices

--- AircraftSystem/Program.cs
+++ AircraftSystem/Program.cs
@@ -1,13 +1,19 @@
-﻿using System.Data.SQLite;
+﻿using AircraftSystem.Models;
+using AircraftSystem;
+using System.Data.SQLite;
 
 //Database object
-var databaseObject = new AircraftSystem.Database();
+AircraftSystem.Database databaseObject = new AircraftSystem.Database();
 
-string queryInsert = "INSERT INTO countries (`shorthand`, `name`, `isEurope`) VALUES (@shorthand, @name, @isEurope)";
-SQLiteCommand myCommand = new SQLiteCommand(queryInsert, databaseObject.myConnection);
-databaseObject.OpenConnection();
-myCommand.Parameters.AddWithValue("@shorthand", "LT");
-myCommand.Parameters.AddWithValue("@name", "Lithuania");
-myCommand.Parameters.AddWithValue("@isEurope", 1);
-var result = myCommand.ExecuteNonQuery(); 
-databaseObject.CloseConnection();
+while (true)
+{
+    switch (Menu.GetActionType())
+    {
+        case ActionType.Report:
+            break;
+        case ActionType.Database:
+            break;
+        case ActionType.Exit:
+            return;
+    }
+}
