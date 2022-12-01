25b708c94b709f7fdb3e671ef224ba464efdeb71
Ausra Bickuviene <bickuviene.ausra@gmail.com>
2022-12-01 21:01:47 +0200

    Added SQLite database "aircraftDB.sqlite"

--- AircraftSystem/Database.cs
+++ AircraftSystem/Database.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Data.SQLite;
 
 namespace AircraftSystem
 {
-    internal class Database
+    public class Database
     {
+        public SQLiteConnection myConnection;
+
+        public Database()
+        {
+            myConnection = new SQLiteConnection("Data Source = aircraftDB.sqlite");
+
+            if (!File.Exists("./aircraftDB.sqlite"))
+            {
+
+                SQLiteConnection.CreateFile("aircraftDB.sqlite");
+                Console.WriteLine("Database created");
+            }
+        }
+
+        public void OpenConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+        }
+        public void CloseConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Closed)
+            {
+                myConnection.Close();
+            }
+        }
     }
 }
--- AircraftSystem/Program.cs
+++ AircraftSystem/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data.SQLite;
+
+//Database object
+var databaseObject = new AircraftSystem.Database();
+
+string queryInsert = "INSERT INTO countries (`shorthand`, `name`, `isEurope`) VALUES (@shorthand, @name, @isEurope)";
+SQLiteCommand myCommand = new SQLiteCommand(queryInsert, databaseObject.myConnection);
+databaseObject.OpenConnection();
+myCommand.Parameters.AddWithValue("@shorthand", "LT");
+myCommand.Parameters.AddWithValue("@name", "Lithuania");
+myCommand.Parameters.AddWithValue("@isEurope", 1);
+var result = myCommand.ExecuteNonQuery(); 
+databaseObject.CloseConnection();
