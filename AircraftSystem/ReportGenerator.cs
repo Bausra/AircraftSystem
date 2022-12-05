9bec985f8756fce90349a9eb56b1d14cc942d82e
Ausra Bickuviene <bickuviene.ausra@gmail.com>
2022-12-05 23:31:52 +0200

    Added ReportGenerator class

---
+++ AircraftSystem/ReportGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AircraftSystem.Models;
+using ConsoleTables;
+
+namespace AircraftSystem
+{
+    public class ReportGenerator
+    {
+        AircraftRepository aircraftRepository;
+        AircraftModelRepository aircraftModelRepository;
+        CompanyRepository companyRepository;
+        CountryRepository countryRepository;
+
+        public ReportGenerator(AircraftRepository aircraftRepository, AircraftModelRepository aircraftModelRepository, CompanyRepository companyRepository, CountryRepository countryRepository)
+        {
+            this.aircraftRepository = aircraftRepository;
+            this.aircraftModelRepository = aircraftModelRepository;
+            this.companyRepository = companyRepository;
+            this.countryRepository = countryRepository;
+        }
+
+        internal void PrintReportTableContent(bool isEurope)
+        {
+            //Console.WriteLine("Aircraft ID | Tail Number | Model Description | Model Number | Company Name | Country Shorthand | Country Name");
+            
+            foreach (Aircraft aircraft in aircraftRepository.GetAllAircraftData())
+            {
+                if (aircraft.Country.IsEurope == isEurope)
+                {
+                    Console.WriteLine($"{aircraft.ID} {aircraft.TailNumber} {aircraft.AircraftModel.Description} {aircraft.AircraftModel.Number} {aircraft.Company.Name} {aircraft.Country.Shorthand} {aircraft.Country.Name}");
+                }
+            }
+        }
+
+        public void GenerateReportAircraftInEurope()
+        {
+            Console.WriteLine("Aircrafts in Europe:");
+
+            PrintReportTableContent(true);
+        }
+
+        public void GenerateReportAircraftNotInEurope()
+        {
+            Console.WriteLine("Aircrafts from outside Europe:");
+
+            PrintReportTableContent(false);
+        }
+    }
+}
